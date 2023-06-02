4ec67a2ac65b0295356bf24e29d98d1b3efbefb1
James Gersbach <jamesgersbach101@gmail.com>
2023-06-02 11:31:49 -0700

    update props for confluence

--- crates/forge_loader/src/forgepermissions.rs
+++ crates/forge_loader/src/forgepermissions.rs
@@ -5,65 +5,65 @@ use serde::{self, Serialize, Deserialize};
 pub enum ForgePermissions {
     #[serde(rename = "read:audit-log:confluence")]
     WriteAuditLogsConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:audit-log:confluence")]
     ReadAuditLogsConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:confluence-content")]
     WriteConfluenceContent,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-space.summary")]
     ReadConfluenceSpaceSummary,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:confluence-space")]
     WriteConfluenceSpace,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:confluence-file")]
     WriteConfluenceFile,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-props")]
     ReadConfluenceProps,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:confluence-props")]
     WriteConfluenceProps,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-content.all")]
     ReadConfluenceContentAll,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-content.summary")]
     ReadConfluenceContentSummary,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:inlinetask:confluence")]
     ReadInlineTaskConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "search:confluence")]
     SearchConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-content.permission")]
     ReadConfluenceContentPermission,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:content-details:confluence")]
     ReadContentDetailsConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-user")]
     ReadConfluenceUser,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:user.property:confluence")]
     ReadUserPropertyConfluence,    
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "manage:confluence-configuration")]
     ManageConfluenceConfiguration,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:content.metadata:confluence")]
     ReadContentMetadataConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:confluence-content.permission")]
     ReadConfluenceGroups,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:confluence-groups")]
     WriteConfluenceGroups,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:user.property:confluence")]
     WriteUserPropertyConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:space.permission:confluence")]
     ReadSpacePermissionConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:space.permission:confluence")]
     WriteSpacePermissionsConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:inlinetask:confluence")]
     WriteInlineTaskConfluence,    
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "readonly:content.attachment:confluence")]
     ReadOnlyContentAttachmentConfluence,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:jira-user")]
     ReadJiraUser,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "read:jira-work")]
     ReadJiraWork,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "write:jira-work")]
     WriteJiraWork,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "manage:jira-project")]
     ManageJiraProject,
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "manage:jira-configuration")]
     ManageJiraConfiguration,    
-    #[serde(rename = "read:audit-log:confluence")]
+    #[serde(rename = "manage:jira-webhook")]
     ManageJiraWebhook,
     #[serde(other)]
     Unknown,
