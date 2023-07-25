d959dbfbef7008d3812ce9e264829a02a0016f08
James Gersbach <jamesgersbach101@gmail.com>
2023-07-25 10:47:58 -0700

    formatting

--- crates/forge_analyzer/src/definitions.rs
+++ crates/forge_analyzer/src/definitions.rs
@@ -2209,7 +2209,7 @@ impl Visit for ExportCollector<'_> {
             if &ident.sym == "module" {
                 if let Some(mem_expr) = mem_expr_from_assign(n) {
                     if let MemberProp::Ident(ident_property) = &mem_expr.prop {
-                        if ident_property.sym.to_string() == "exports" {
+                        if &ident_property.sym == "exports" {
                             match &*n.right {
                                 Expr::Fn(FnExpr { ident, function }) => self.add_default(
                                     DefRes::Function(()),
@@ -2228,7 +2228,7 @@ impl Visit for ExportCollector<'_> {
                 if let Some(mem_expr) = mem_expr_from_assign(n) {
                     if let MemberProp::Ident(ident_property) = &mem_expr.prop {
                         //self.add_export(DefRes::Undefined, ident_property.to_id());
-                        // TODO: handling aliases 
+                        // TODO: handling aliases
                         match &*n.right {
                             Expr::Fn(FnExpr { ident, function }) => {
                                 self.add_export(DefRes::Function(()), ident_property.to_id());
