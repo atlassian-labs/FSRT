dae77d6b2a36e2bbcf74c8154b072849a8189fec
Joshua Wong <joshuawong@anticentri.st>
2023-09-07 15:32:22 -0500

    fix: correctly lower process['env']

--- crates/forge_analyzer/src/utils.rs
+++ crates/forge_analyzer/src/utils.rs
@@ -1,5 +1,5 @@
 use crate::definitions::CalleeRef;
-use swc_core::ecma::ast::{Expr, MemberProp};
+use swc_core::ecma::ast::{Expr, Lit, MemberProp};
 
 pub fn calls_method(n: CalleeRef<'_>, name: &str) -> bool {
     if let CalleeRef::Expr(Expr::Member(mem)) = &n {
@@ -8,4 +8,15 @@ pub fn calls_method(n: CalleeRef<'_>, name: &str) -> bool {
         }
     }
     false
+}
+
+pub fn eq_prop_name(n: &MemberProp, name: &str) -> bool {
+    match n {
+        MemberProp::Ident(ident) => ident.sym == *name,
+        MemberProp::Computed(expr) => match expr.expr.as_ref() {
+            Expr::Lit(Lit::Str(lit)) => *lit.value == *name,
+            _ => false,
+        },
+        _ => false,
+    }
 }
