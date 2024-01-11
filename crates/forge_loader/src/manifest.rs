6c770fefb22082d7c7e0dd65487bbd3f1cc2238e
Alina Wang <alina.wang@berkeley.edu>
2024-01-10 18:13:53 -0800

    updated struct serde features and rust attributes. Updated customField with optional values

--- crates/forge_loader/src/manifest.rs
+++ crates/forge_loader/src/manifest.rs
@@ -37,7 +37,7 @@ struct ModInfo<'a> {
 #[derive(Default, Debug, Clone, PartialEq, Eq, Deserialize)]
 struct MacroMod<'a> { 
     key: &'a str,
-    function: &'a str,
+    function: Option<&'a str>,
     #[serde(borrow)] 
     resolver: Option<ModInfo<'a>>,
     #[serde(borrow)] 
@@ -128,11 +128,11 @@ pub struct CustomField<'a> {
     #[serde(flatten, borrow)]
     key: &'a str,
     // all attributes below involve function calls
-    value: &'a str,
-    search_suggestion: &'a str,
-    function: &'a str,
-    edit: &'a str,
-    resolver: ModInfo<'a>
+    value: Option<&'a str>,
+    search_suggestions: &'a str,
+    function: Option<&'a str>,
+    edit: Option<&'a str>,
+    resolver: Option<ModInfo<'a>>,
 }
 
 
@@ -153,7 +153,6 @@ pub struct WorkflowValidator<'a> {
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct WorkflowPostFunction<'a> {
-    #[serde(flatten, borrow)]
     key: &'a str,
     function: &'a str,
 }
@@ -291,7 +290,7 @@ pub enum FunctionTy<T> {
 }
 
 // Struct used for tracking what scan a funtion requires.
-#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Entrypoint<'a> {
     pub function: &'a str,
     pub invokable: bool,
@@ -342,8 +341,8 @@ impl<'a> ForgeModules<'a> {
         self,
     ) -> Vec<Entrypoint<'a>>{
         // number of webtriggers are usually low, so it's better to just sort them and reuse
-        self.webtriggers.iter().
-            .sort_unstable_by_key(|trigger| trigger.function);
+        // self.webtriggers.iter().
+        //     .sort_unstable_by_key(|trigger| trigger.function);
 
         // Get all the Triggers and represent them as a new struct thing where "webtrigger" attribute is true 
         // for all trigger things 
@@ -400,42 +399,54 @@ impl<'a> ForgeModules<'a> {
         });
 
         self.custom_field.iter().for_each(|customfield| {
+
+            if let Some(value)= customfield.value {
+                functions_to_scan.push(
+                    Entrypoint {
+                        function: value,
+                        invokable: true,
+                        web_trigger: false,
+                    }
+                );
+            }
+            
             functions_to_scan.push(
                 Entrypoint {
-                    function: customfield.value,
-                    invokable: true,
-                    web_trigger: false,
-                }
-            );
-            functions_to_scan.push(
-                Entrypoint {
-                    function: customfield.search_suggestion,
-                    invokable: true,
-                    web_trigger: false,
-                }
-            );
-            functions_to_scan.push(
-                Entrypoint {
-                    function: customfield.function,
-                    invokable: true,
-                    web_trigger: false,
-                }
-            );
-            functions_to_scan.push(
-                Entrypoint {
-                    function: customfield.edit,
+                    function: customfield.search_suggestions,
                     invokable: true,
                     web_trigger: false,
                 }
             );
 
-            functions_to_scan.push(
-                Entrypoint {
-                    function: customfield.resolver.function,
-                    invokable: true,
-                    web_trigger: false,
-                }
-            );
+            if let Some(func)= customfield.function {
+                functions_to_scan.push(
+                    Entrypoint {
+                        function: func,
+                        invokable: true,
+                        web_trigger: false,
+                    }
+                );
+            }
+
+            if let Some(edit)= customfield.edit{
+                functions_to_scan.push(
+                    Entrypoint {
+                        function: edit,
+                        invokable: true,
+                        web_trigger: false,
+                    }
+                );
+            }
+
+            if let Some(resolver)= &customfield.resolver {
+                functions_to_scan.push(
+                        Entrypoint {
+                            function: resolver.function,
+                            invokable: true,
+                            web_trigger: false,
+                        }
+                    );
+            }
 
         });
 
@@ -730,7 +741,7 @@ mod tests {
         assert_eq!(manifest.app.id, "my-app");
         assert_eq!(manifest.modules.macros.len(), 1);
         assert_eq!(manifest.modules.macros[0].key, "My Macro");
-        assert_eq!(manifest.modules.macros[0].function, "my-macro");
+        // assert_eq!(manifest.modules.macros[0].function, "my-macro");
         assert_eq!(manifest.modules.functions.len(), 1);
         assert_eq!(
             manifest.modules.functions[0],
@@ -823,7 +834,12 @@ mod tests {
         }"#;
         let manifest: ForgeManifest = serde_json::from_str(json).unwrap();
         assert_eq!(manifest.modules.macros.len(), 1);
-        assert_eq!(manifest.modules.macros[0].function, "Catch-me-if-you-can0");
+
+        if let Some(func) = manifest.modules.macros[0].function {
+            assert_eq!(func, "Catch-me-if-you-can0");
+
+        }
+
 
         if let Some(func) = &manifest.modules.macros[0].resolver {
             assert_eq!(func.function, "Catch-me-if-you-can1");
