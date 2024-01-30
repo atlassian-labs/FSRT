4654e39cf4e5dc009e98c37a8df728e809b1ea0b
Joshua Wong <joshuawong@anticentri.st>
2024-01-30 11:27:28 -0600

    chore: fix warnings

--- crates/forge_loader/src/manifest.rs
+++ crates/forge_loader/src/manifest.rs
@@ -5,7 +5,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::{forgepermissions::ForgePermissions, Error};
+use crate::Error;
 use forge_utils::FxHashMap;
 use itertools::{Either, Itertools};
 use serde::Deserialize;
