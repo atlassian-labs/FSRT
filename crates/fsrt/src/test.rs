6aaccc8a16882ca8082a34a0576a2853e5bea730
gersbach <jgersbach@atlassian.com>
2024-05-13 13:04:53 -0700

    removing redundant rust module

--- crates/fsrt/src/test.rs
+++ crates/fsrt/src/test.rs
@@ -1,62 +1,61 @@
-mod tests {
-    use crate::{scan_directory_test, MockForgeProject};
-    use forge_loader::manifest::{ForgeManifest, FunctionMod};
-    use std::{collections::HashMap, path::PathBuf};
+use crate::{scan_directory_test, MockForgeProject};
+use forge_loader::manifest::{ForgeManifest, FunctionMod};
+use std::{collections::HashMap, path::PathBuf};
 
-    #[test]
-    fn test_simple() {
-        let forge_manifest = ForgeManifest::create_manifest_with_func_mod(FunctionMod {
-            key: "main",
-            handler: "index.run",
-            providers: None,
-        });
+#[test]
+fn test_simple() {
+    let forge_manifest = ForgeManifest::create_manifest_with_func_mod(FunctionMod {
+        key: "main",
+        handler: "index.run",
+        providers: None,
+    });
 
-        let mut test_forge_project = MockForgeProject {
-            test_manifest: forge_manifest,
-            files_name_to_source: HashMap::new(),
-        };
-        test_forge_project.files_name_to_source.insert(
-            PathBuf::from("src/index.tsx"),
-            "import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui'; \n
+    let mut test_forge_project = MockForgeProject {
+        test_manifest: forge_manifest,
+        files_name_to_source: HashMap::new(),
+    };
+    test_forge_project.files_name_to_source.insert(
+        PathBuf::from("src/index.tsx"),
+        "import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui'; \n
             function App() { console.log('test') } \n
             export const run = render(<Macro app={<App />} />);"
-                .to_string(),
-        );
+            .to_string(),
+    );
 
-        let scan_result = scan_directory_test(test_forge_project);
-        assert!(scan_result.is_some_and(|vec| vec.is_empty()));
-    }
+    let scan_result = scan_directory_test(test_forge_project);
+    assert!(scan_result.is_some_and(|vec| vec.is_empty()));
+}
 
-    #[test]
-    fn test_secret_vuln() {
-        let forge_manifest = ForgeManifest::create_manifest_with_func_mod(FunctionMod {
-            key: "main",
-            handler: "index.run",
-            providers: None,
-        });
+#[test]
+fn test_secret_vuln() {
+    let forge_manifest = ForgeManifest::create_manifest_with_func_mod(FunctionMod {
+        key: "main",
+        handler: "index.run",
+        providers: None,
+    });
 
-        let mut test_forge_project = MockForgeProject {
-            test_manifest: forge_manifest,
-            files_name_to_source: HashMap::new(),
-        };
-        test_forge_project.files_name_to_source.insert(
-            PathBuf::from("src/index.tsx"),
-            "import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
+    let mut test_forge_project = MockForgeProject {
+        test_manifest: forge_manifest,
+        files_name_to_source: HashMap::new(),
+    };
+    test_forge_project.files_name_to_source.insert(
+        PathBuf::from("src/index.tsx"),
+        "import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
              import jwt from 'jsonwebtoken';
             function App() { console.log('test');
                 jwt.sign({}, 'TEST');
              } \n
             export const run = render(<Macro app={<App />} />);"
-                .to_string(),
-        );
+            .to_string(),
+    );
 
-        let scan_result = scan_directory_test(test_forge_project);
-        assert!(scan_result.is_some_and(|vec| vec.is_empty()));
-    }
+    let scan_result = scan_directory_test(test_forge_project);
+    assert!(scan_result.is_some_and(|vec| vec.is_empty()));
+}
 
-    #[test]
-    fn with_multiple_files() {
-        let test_forge_project = MockForgeProject::files_from_string(
+#[test]
+fn with_multiple_files() {
+    let test_forge_project = MockForgeProject::files_from_string(
             "// src/index.tsx
             import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui'; \n
             import test_function from 'test_function'; \n
@@ -67,7 +66,6 @@ mod tests {
                 .to_string(),
         );
 
-        let scan_result = scan_directory_test(test_forge_project);
-        assert!(scan_result.is_some_and(|vec| vec.is_empty()));
-    }
+    let scan_result = scan_directory_test(test_forge_project);
+    assert!(scan_result.is_some_and(|vec| vec.is_empty()));
 }
