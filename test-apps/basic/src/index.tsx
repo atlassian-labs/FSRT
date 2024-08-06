25973c47779fb511da2c44f42ce0e51a9ff960a2
Jessie Xu <jxu5@h43x974vgd.office.atlassian.com>
2024-08-06 12:54:30 -0700

    more cases w/ while loops and conds

--- test-apps/basic/src/index.tsx
+++ test-apps/basic/src/index.tsx
@@ -58,32 +58,87 @@ function compnewfunc3() {
   let c = a;  // bb2
 }
 
-function loop() {
-  let a = 0;
-  for (let i = 0; i < 10; i++) {
-    a = a + i;
-  }
-  return a;
-}
+// function forloop() {
+//   let a = 0;
+//   for (let i = 0; i < 10; i++) {
+//     a = a + i;
+//   }
+//   return a;
+// }
 
 function whileloop() {
   let a = 0;
   let i = 0;
   while (i < 10) {
-    a = a + i;
+    a = a + i;  // bb3
     i++;
   }
   return a;
 }
 
-function doubleloop() {
-  let a = 0;  // bb0
-  for (let i = 0; i < 10; i++) {  // i = 0 is in bb0; cond in bb1; i++ in bb4
-    for (let j = 0; j < 10; j++) {  // j = 0 in bb3; cond in bb4
-      a = a + i + j;  //bb6
+function nestedwhileloop() {
+  let a = 0;
+  let i = 0;
+  while (i < 10) {
+    let j = 0;
+    while (j < 10) {
+      a = a + i + j;
+      j++;
     }
-    let b = 3;
+    i++;
+  }
+  return a;
+}
+
+function whileandcond() {
+  let a = 0;
+  let i = 0;  // bb0
+  while (i < 10) {  // bb1
+    if (i > 5) {  // bb3
+      a = a + i;  // bb4
+    }
+    i++;  // bb5
   }
   return a;  // bb2
 }
+
+function whileandcond2() {
+  let a = 0;
+  let i = 0;  // bb0
+  while (i < 10) {  // bb1
+    if (i > 5) {  // bb3
+      a = a + 1;  // bb4
+    } else {
+      a = a + 2;  // bb6
+    }
+    i++;  // bb5
+  }
+  return a;  // bb2
+}
+
+function whileincond() {
+  let a = 0;
+  let b = Math.random();  // bb0
+  if (b < 0.5) {  // bb1
+    while (a < 2) {  // bb4
+      a = a + b;  // bb6
+    } 
+                  // bb5
+  } else {
+    a = 1;  // bb3
+  }
+  return a;  // bb2
+}
+
+// ignore below for now
+// function nestedforloop() {
+//   let a = 0;  // bb0
+//   for (let i = 0; i < 10; i++) {  // i = 0 is in bb0; cond in bb1; i++ in bb4
+//     for (let j = 0; j < 10; j++) {  // j = 0 in bb3; cond in bb4
+//       a = a + i + j;  //bb6
+//     }
+//     let b = 3;
+//   }
+//   return a;  // bb2
+// }
 
