3a1ea31425ece03e9c85aeaacef5509ca1060481
Jessie Xu <jxu5@h43x974vgd.office.atlassian.com>
2024-08-29 09:34:44 -0700

    adding dominators - dom tree, dom frontier

--- test-apps/basic/src/index.tsx
+++ test-apps/basic/src/index.tsx
@@ -1,3 +1,123 @@
+// function newfunc() {
+//   let a = 3;
+//   if (a > 2) {
+//     a = 1;
+//     let b = a;
+//   }  
+//   let c = a;
+// }
+
+// function compnewfunc2() {
+//   let a = Math.random();
+//   let b = 0;
+//   if (a >= 0.5) {
+//     if (a > 0.5) {
+//       a = a + 1;
+//     } else {
+//       a = a + 2;
+//     }
+//     b = a + 3;
+//   }
+//   let c = a;
+// }
+
+// function compnewfunc3() {
+//   let a = Math.random();  // bb0
+//   let b = 0;
+//   if (a >= 0.5) {
+//     if (a > 0.5) { // bb1
+//       a = a + 1;   // bb4
+//     } else {
+//       a = a + 2;  // bb6
+//     }
+//     b = a + 3;  // bb5
+//   } else {  // bb3
+//     b = a + 4;
+//   }
+//   let c = a;  // bb2
+// }
+
+// // function forloop() {
+// //   let a = 0;
+// //   for (let i = 0; i < 10; i++) {
+// //     a = a + i;
+// //   }
+// //   return a;
+// // }
+
+// function whileloop() {
+//   let a = 0;
+//   let i = 0;
+//   while (i < 10) {
+//     a = a + i;  // bb3
+//     i = i + 1;
+//   }
+//   return a;
+// }
+
+// function nestedwhileloop() {
+//   let a = 0;
+//   let i = 0;
+//   while (i < 10) {
+//     let j = 0;
+//     while (j < 10) {
+//       a = a + i + j;
+//       j = j + 1;
+//     }
+//     i = i + 1;
+//   }
+//   return a;
+// }
+
+// function whileandcond() {
+//   let a = 0;
+//   let i = 0;  // bb0
+//   while (i < 10) {  // bb1
+//     if (i > 5) {  // bb3
+//       a = a + i;  // bb4
+//     }
+//     i = i + 1;  // bb5
+//   }
+//   return a;  // bb2
+// }
+
+// function whileandcond2() {
+//   let a = 0;
+//   let i = 0;  // bb0
+//   while (i < 10) {  // bb1
+//     if (i > 5) {  // bb3
+//       a = a + 1;  // bb4
+//     } else {
+//       a = a + 2;  // bb6
+//     }
+//     i = i + 1;  // bb5
+//   }
+//   return a;  // bb2
+// }
+
+// function whileincond() {
+//   let a = 0;
+//   let b = Math.random();  // bb0
+//   if (b < 0.5) {  // bb1
+//     while (a < 2) {  // bb4
+//       a = a + b;  // bb6
+//     } 
+//                   // bb5
+//   } else {
+//     a = 1;  // bb3
+//   }
+//   return a;  // bb2
+// }
+
+// function earlyret() {
+//   let a = 0; 
+//   if (a > 5) {  // bb0
+//     a = 1;
+//     return a;  // bb1
+//   }
+//   return a;  // bb2
+// }
+
 import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
 import api, { route, fetch } from '@forge/api';
 import { testFn } from './test';
