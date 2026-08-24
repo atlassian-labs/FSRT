24e40cd0be47b40bf8a1e669f5c55f39cf29e235
David Detweiler <ddetweiler@atlassian.com>
2026-08-24 11:35:48 -0700

    Support break statements in switch statements

--- crates/forge_analyzer/src/interp.rs
+++ crates/forge_analyzer/src/interp.rs
@@ -1240,7 +1240,6 @@ impl<'cx, C: Runner<'cx>> Interp<'cx, C> {
     }
 
     // Understand how this works, write a spec / tickets for what dataflow would look like
-    // F*#$ing implement it
     pub fn try_check_function(&mut self, def: DefId, checker: &mut C) -> Result<(), Error> {
         let resolved_def = self.env.resolve_alias(def);
         let name = self.env.def_name(resolved_def);
